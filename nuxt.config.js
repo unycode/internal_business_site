53fd341bd7445baddac23431a4a209118f9c1206
Jordaine Gayle <freshcode9@gmail.com>
2022-10-31 01:31:45 -0500

    - updated head

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "Unycode Business Website",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content: "Solving all the world's software problems together."
       }
     ],
     bodyAttrs: {
